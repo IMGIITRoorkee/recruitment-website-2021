9fda0aa85fa92c0a6dd259271832a4f3e743c02a
Shivansh Bindal <35252539+shivanshbindal9@users.noreply.github.com>
2019-11-30 01:03:36 +0530

    minor fixes in header (#4)

--- js/fix_header.js
+++ js/fix_header.js
@@ -4,7 +4,9 @@ function myFunction() {
     let navbar=document.getElementById("site-navbar");
     if (window.pageYOffset>0) {
         navbar.classList.add("sticky");
+        document.getElementById("sidebarMenu").style.marginTop="40px";
     } else {
         navbar.classList.remove("sticky");
+        document.getElementById("sidebarMenu").style.marginTop="53px";
     }
 }
