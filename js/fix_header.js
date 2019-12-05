389172f848a219922a7d9b6eaf4512648e81e312
Aman Sharma <mannu.poski10@gmail.com>
2019-12-05 20:09:59 +0530

    Reduce margin between sidebar and the header

--- js/fix_header.js
+++ js/fix_header.js
@@ -4,9 +4,9 @@ function myFunction() {
     let navbar=document.getElementById("site-navbar");
     if (window.pageYOffset>0) {
         navbar.classList.add("sticky");
-        document.getElementById("sidebarMenu").style.marginTop="40px";
+        document.getElementById("sidebarMenu").style.marginTop="39px";
     } else {
         navbar.classList.remove("sticky");
-        document.getElementById("sidebarMenu").style.marginTop="53px";
+        document.getElementById("sidebarMenu").style.marginTop="54px";
     }
 }
