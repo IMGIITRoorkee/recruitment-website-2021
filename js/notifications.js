9ea97d5b9e97db55a52b2b67cac02beea7a6af00
pradumangoyal <goyal.praduman611@gmail.com>
2019-12-10 14:57:17 +0530

    Add notifications

---
+++ js/notifications.js
@@ -0,0 +1,58 @@
+
+const notificationArray = [
+    { title: 'IMG Recruitment Update', body: 'Hey, have you checked out the Winter Design Assignment? Click here to check them now.', icon: '/assets/img_notif.png', redirectURL: '/design/', token: 'eflzPOEBN3' },
+    { title: 'IMG Recruitment Update', body: 'Hey, have you checked out the Winter Developer Assignment? Click here to check them now.', icon: '/assets/img_notif.png', redirectURL: '/development/', token: 'LeyjfMSaDg' },
+]
+
+
+// Cookies, yumm!
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+function spawnNotification() {
+    const notifLastToken = getCookie('notifLastToken')
+    let lastNotifIndex = -1
+    lastNotifIndex = notificationArray.findIndex(notif => (notif.token == notifLastToken))
+    const notification = []
+    for (let i = lastNotifIndex + 1; i < notificationArray.length; i++) {
+        setCookie('notifLastToken', notificationArray[i].token, 90)
+        notification[i] = new Notification(notificationArray[i].title, { body: notificationArray[i].body, icon: notificationArray[i].icon })
+        if (notificationArray[i].redirectURL) {
+            notification[i].onclick = () => {
+                notification[i].close()
+                window.location = notificationArray[i].redirectURL
+            }
+        }
+    }
+}
+
+const notifPermission = Notification.permission
+
+
+if (notifPermission != "granted") {
+    Notification.requestPermission().then(function (result) {
+        spawnNotification()
+    })
+} else {
+    spawnNotification()
+}
