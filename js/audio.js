08a4deb6b719341a21dbe18c0decddbb04871ed7
Saurabh Rana <saurabhsinghrana70@gmail.com>
2024-12-23 14:42:56 +0530

    Updates icons and syncs media controls

--- js/audio.js
+++ js/audio.js
@@ -4,9 +4,23 @@ function toggleAudio() {
     audio.loop = true;
     if (audio.paused) {
       audio.play();
-      icon.src = '/assets/mute-icon.png';
-    } else {
+      icon.src = '/assets/pause-fill.svg';
+    }
+    else {
       audio.pause();
-      icon.src = '/assets/play-icon.png';
+      icon.src = '/assets/play-fill.svg';
     }
   }
+
+// Listen for keyboard play/pause events
+navigator.mediaSession.setActionHandler('play', () => {
+  const audio = document.getElementById('batman-theme');
+  audio.play();
+  document.getElementById('audio-icon').src = '/assets/pause-fill.svg';
+});
+
+navigator.mediaSession.setActionHandler('pause', () => {
+  const audio = document.getElementById('batman-theme');
+  audio.pause();
+  document.getElementById('audio-icon').src = '/assets/play-fill.svg';
+});
